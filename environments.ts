a6eb438905e0a747359dc99db2a9ce42662dffbe
Stephen John Raymundo <StephenJohn.Raymundo@finastra.com>
2020-08-20 17:29:51 +0800

    add setup files

---
+++ environments.ts
@@ -0,0 +1,23 @@
+export const environments = {
+    dev: {
+        apiKey: "AIzaSyA8Y7RE-M3gFAk3wtQ02ulrq0p3aaVyOLw",
+        authDomain: "sjtodo-dev.firebaseapp.com",
+        databaseURL: "https://sjtodo-dev.firebaseio.com",
+        projectId: "sjtodo-dev",
+        storageBucket: "sjtodo-dev.appspot.com",
+        messagingSenderId: "245542258761",
+        appId: "1:245542258761:web:79f125072dbd5d8f2c5798",
+        measurementId: "G-N5Z5NZS0SQ"
+    },
+    prod: {
+        apiKey: "AIzaSyA8Y7RE-M3gFAk3wtQ02ulrq0p3aaVyOLw",
+        authDomain: "sjtodo-dev.firebaseapp.com",
+        databaseURL: "https://sjtodo-dev.firebaseio.com",
+        projectId: "sjtodo-dev",
+        storageBucket: "sjtodo-dev.appspot.com",
+        messagingSenderId: "245542258761",
+        appId: "1:245542258761:web:79f125072dbd5d8f2c5798",
+        measurementId: "G-N5Z5NZS0SQ"
+    }
+}
+  
